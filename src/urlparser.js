aaec24c414f1bcfa702e85d8f90edbfb7cdce6e7
johnwilander <john.wilander@gmail.com>
2013-03-26 15:36:24 +0100

    Naïve parsing of a URL without explicit port, query parameters, and fragment working. TDDing.

--- src/urlparser.js
+++ src/urlparser.js
@@ -8,16 +8,20 @@
     global.urlParser = (function() {
 
         var _extractScheme = function (parsedSegments) {
-                var split = parsedSegments.rest.split(':'),
-                    // shift() removes the first item and returns it
-                    scheme = split.shift();
-                parsedSegments.scheme = scheme;
-                parsedSegments.rest = split.join(':');
+                var toExtractFrom = parsedSegments.rest,
+                    firstHit = toExtractFrom.indexOf(':');
+                parsedSegments.scheme = toExtractFrom.substring(0, firstHit);
+                parsedSegments.rest = toExtractFrom.substring(firstHit + 1);  // Skip the ':'
                 return parsedSegments;
             },
 
-            _extractDoubleSlash = function (segments) {
-                return segments;
+            _extractDoubleSlash = function (parsedSegments) {
+                var toLookFor = '//',
+                    toExtractFrom = parsedSegments.rest,
+                    firstHit = toExtractFrom.indexOf(toLookFor);
+                parsedSegments.doubleSlash = firstHit !== -1;
+                parsedSegments.rest = toExtractFrom.substring(firstHit + toLookFor.length);  // Skip the '//'
+                return parsedSegments;
             },
 
             _extractCredentialsFromAuthority = function (segments) {
@@ -28,14 +32,40 @@
                 return segments;
             },
 
-            _extractAuthority = function (segments) {
-                segments = _extractCredentialsFromAuthority(segments);
-                segments = _extractAddressFromAuthority(segments);
-                return segments;
+            _firstIndexOf = function(toSearchIn, searchValues) {
+                var firstIndex = toSearchIn.length,  // Impossible value
+                    currentIndexOf;
+
+                for (var i = 0; i < searchValues.length; i++) {
+                    currentIndexOf = toSearchIn.indexOf(searchValues[i]);
+                    if (currentIndexOf !== -1 && currentIndexOf < firstIndex) {
+                        firstIndex = currentIndexOf;
+                    }
+                }
+
+                if (firstIndex < toSearchIn.length) {
+                    return firstIndex;
+                } else {
+                    return -1;
+                }
             },
 
-            _extractPath = function (segments) {
-                return segments;
+            _extractAuthority = function (parsedSegments) {
+                var authority, indexOf,
+                    toExtractFrom = parsedSegments.rest;
+
+                indexOf = _firstIndexOf(toExtractFrom, ['/', '?', '#']);
+                authority = toExtractFrom.substring(0, indexOf);
+                parsedSegments.address = authority;
+                parsedSegments.rest = toExtractFrom.substring(indexOf);
+
+                return parsedSegments;
+            },
+
+            _extractPath = function (parsedSegments) {
+                parsedSegments.path = parsedSegments.rest;
+                parsedSegments.rest = "";
+                return parsedSegments;
             },
 
             _extractQueryString = function (segments) {
