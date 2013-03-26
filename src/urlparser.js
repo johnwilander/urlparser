3949ff88898fc92fc72373bc7bc4b826d37b9dcf
johnwilander <john.wilander@gmail.com>
2013-03-26 11:52:34 +0100

    Skeleton version of parser function including a first test case. Throws error for all input.

--- src/urlparser.js
+++ src/urlparser.js
@@ -1,5 +1,28 @@
-(function () {
+(function (global) {
    "use strict";
 
+    /**
+     *
+     * @type {object}
+     */
+    global.urlParser = (function() {
+        var _parse = function (urlString) {
+            throw new Error("Invalid URL: " + urlString);
+        };
 
-}());
+        return {
+            /**
+             * Function parsing URLs.
+             * Has no side effects or state.
+             *
+             * @param {String} urlString URL to parse
+             * @return {Url} a URL object
+             */
+            parse : function(urlString) {
+                var segments = _parse(urlString);
+                return new Url(segments);
+            }
+        };
+    }());
+
+}(window));
--- test/urlparserTestSpec.js
+++ test/urlparserTestSpec.js
@@ -1,7 +1,21 @@
 describe("Tests of urlparser", function () {
 
-    it.skip("test goes here…", function () {
+    describe("Tests of the parse() function", function () {
+        it("should throw an error for empty URL strings", function () {
+            // given
+            var urlString = "",
+                errorCaught = false,
+                url;
 
+            // when
+            try {
+                url = urlParser.parse(urlString);
+            } catch (error) {
+                errorCaught = true;
+            }
+
+            //then
+            expect(errorCaught).to.be(true);
+        });
     });
-
 });
