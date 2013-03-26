206e2ab754a5d56ab831e83228fb14b268f9dbc3
johnwilander <john.wilander@gmail.com>
2013-03-26 12:16:31 +0100

    Failing test for first real URL. Also described test cases not implemented yet.

--- test/urlparserTestSpec.js
+++ test/urlparserTestSpec.js
@@ -1,5 +1,9 @@
 describe("Tests of urlparser", function () {
 
+    var schemes = ["http", "https", "ftp", "file", "data"],
+        gTLDs   = ["aero", "asia", "biz", "cat", "com", "coop", "edu", "gov", "info", "int", "jobs", "mil", "mobi",
+                   "museum", "name", "net", "org", "post", "pro", "tel", "travel", "xxx"];
+
     describe("Tests of the parse() function", function () {
         it("should throw an error for empty URL strings", function () {
             // given
@@ -17,5 +21,37 @@ describe("Tests of urlparser", function () {
             //then
             expect(errorCaught).to.be(true);
         });
+
+        describe("Tests of absolute URLs with the ASCII character set", function () {
+            it("should be able to parse a URL with scheme, double slash, and address");
+
+            it("should be able to parse a URL with scheme, double slash, address, and path", function () {
+                // given
+                var urlString = "https://github.com/johnwilander/urlparser/blob/master/src/Url.js",
+                    url;
+
+                // when
+                url = urlParser.parse(urlString);
+
+                // then
+                expect(url).to.be.an('object');
+
+                expect(url.scheme).to.be.equal('https');
+                expect(url.doubleSlash).to.be.equal(true);
+                expect(url.username).to.be.equal('');
+                expect(url.password).to.be.equal('');
+                expect(url.address).to.be.equal('github.com');
+                expect(url.port).to.be.equal('');
+                expect(url.path).to.be.equal('/johnwilander/urlparser/blob/master/src/Url.js');
+                expect(url.queryString).to.be.equal('');
+                expect(url.fragment).to.be.equal('');
+
+            });
+
+            it("should be able to parse a URL with scheme, double slash, address, path, and query string");
+            it("should be able to parse a URL with scheme, double slash, address, path, query string, and fragment");
+            it("should be able to parse a URL with scheme, double slash, address, path, and fragment");
+        });
+
     });
 });
