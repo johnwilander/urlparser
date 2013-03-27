e61b7b73e67da9070bea6dd04e0d5679cea96a37
Mattias Karlsson <enkidude@gmail.com>
2013-03-27 17:00:40 +0100

    Added a Gruntfile.js (with support for concat and uglify) and updated package.json with all grunt dependencies. Also added an initial version of a README.md file.

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                // define a string to put between each file in the concatenated output
+                separator: ';'
+            },
+            dist: {
+                // the files to concatenate
+                src: ['src/**/*.js'],
+                // the location of the resulting JS file
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'dist/<%= pkg.name %>.js',
+                dest: 'dist/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+
+    // Load the plugin that provides the "concat" task.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['concat', 'uglify']);
+
+};
---
+++ README.md
@@ -0,0 +1,14 @@
+urlparser
+=========
+
+Installation
+------------
+
+Install grunt dependencies using npm:
+
+    npm install
+
+Install project dependencies using bower:
+
+    bower install
+
